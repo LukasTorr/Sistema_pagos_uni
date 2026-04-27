84ff79ffeca37be15330c055664436d7f535ed44
aklisesegovia <132787660+aklisesegovia@users.noreply.github.com>
2026-04-26 23:25:34 -0400

    Back-Kalise-jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  iniciarSesion(@Body() body: any) {
+    // En un caso real, usaríamos un LoginDto aquí para validar
+    return this.authService.login(body.usuario, body.contrasena);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🔐 VALIDACIONES GLOBALES (MUY IMPORTANTE)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina campos que no están en el DTO
+      forbidNonWhitelisted: true, // lanza error si envían campos extra
+      transform: true, // transforma tipos automáticamente (string → number)
+    }),
+  );
 
   // --- CONFIGURACIÓN DE SWAGGER ---
   const config = new DocumentBuilder()
     .setTitle('Sistema de Pagos NYU - API Central')
     .setDescription('Documentación para que los otros módulos se conecten a la central de pagos.')
     .setVersion('1.0')
-    .addBearerAuth() // Esto crea el botón del "Candadito" para poner el Token
+    .addBearerAuth() // candado JWT
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
-  // La documentación estará disponible en la ruta localhost:3000/api
+
+  // 📌 Ruta: http://localhost:3000/api
   SwaggerModule.setup('api', app, document); 
 
   await app.listen(3000);
