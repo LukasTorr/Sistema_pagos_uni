4810c9e3eda3849359c0f92c1ae899cb92892b35
Kary <karytudela@gmail.com>
2026-06-22 00:23:53 -0400

    Update payment.service.ts

--- frontend/src/app/modules/payment/services/payment.service.ts
+++ frontend/src/app/modules/payment/services/payment.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  private readonly apiUrl =
+    'http://localhost:3000/v1/payments';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  createOrder(
+    payload: any
+  ): Observable<any> {
+
+    return this.http.post(
+      `${this.apiUrl}/orders`,
+      payload
+    );
+  }
+
+  getPayment(
+    referenceId: string
+  ): Observable<any> {
+
+    return this.http.get(
+      `${this.apiUrl}/${referenceId}`
+    );
+  }
+
+  confirmPayment(
+    referenceId: string
+  ): Observable<any> {
+
+    return this.http.patch(
+      `${this.apiUrl}/${referenceId}/confirm`,
+      {}
+    );
+  }
 }
